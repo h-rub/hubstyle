095ad94f6fb002d6041892db13f1e6fc567fa448
h-rub <rubio@loopgk.com>
2022-07-06 12:04:31 -0500

    Layout básico para dashboard. (Pendiente eliminar componentes)

--- src/partials/dashboard/WelcomeBanner.jsx
+++ src/partials/dashboard/WelcomeBanner.jsx
@@ -48,8 +48,8 @@ function WelcomeBanner() {
 
       {/* Content */}
       <div className="relative">
-        <h1 className="text-2xl md:text-3xl text-slate-800 font-bold mb-1">Good afternoon, Acme Inc. 👋</h1>
-        <p>Here is what’s happening with your projects today:</p>
+        <h1 className="text-2xl md:text-3xl text-slate-800 font-bold mb-1">Bienvenido 👋</h1>
+        <p>Este es el layout básico para Dashboards en Hubmine</p>
       </div>
 
     </div>
