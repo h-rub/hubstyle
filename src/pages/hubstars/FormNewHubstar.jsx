5a70e68cf77d95b9b16cceafc83cd1019a44cdf5
Omar Barragan <flaky_11@hotmail.com>
2022-07-12 14:03:03 -0500

    modal en boton cancelar añadido

--- src/pages/hubstars/FormNewHubstar.jsx
+++ src/pages/hubstars/FormNewHubstar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import './styleDate.css';
+import ModalConfirm from '../../../helpers/ModalConfirm';
 import ModalBlank from '../../components/ModalBlank';
 
 function FormNewHubstar() {
@@ -31,66 +32,6 @@ function FormNewHubstar() {
       });
   };
   console.log(infoModalOpen);
-  const prueba = (infoModalOpen) => {
-    infoModalOpen && (
-      <div
-        className='m-1.5'
-        onChnge={(e) => {
-          e.stopPropagation();
-          setInfoModalOpen(true);
-        }}>
-        {/* Start */}
-        <ModalBlank
-          id='info-modal'
-          modalOpen={infoModalOpen}
-          setModalOpen={setInfoModalOpen}>
-          <div className='p-5 flex space-x-4'>
-            {/* Icon */}
-            <div className='w-10 h-10 rounded-full flex items-center justify-center shrink-0 bg-indigo-100'>
-              <svg
-                className='w-4 h-4 shrink-0 fill-current text-primary'
-                viewBox='0 0 16 16'>
-                <path d='M8 0C3.6 0 0 3.6 0 8s3.6 8 8 8 8-3.6 8-8-3.6-8-8-8zm1 12H7V7h2v5zM8 6c-.6 0-1-.4-1-1s.4-1 1-1 1 .4 1 1-.4 1-1 1z' />
-              </svg>
-            </div>
-            {/* Content */}
-            <div>
-              {/* Modal header */}
-              <div className='mb-2'>
-                <div className='text-lg font-semibold text-slate-800'>
-                  Create new Event?
-                </div>
-              </div>
-              {/* Modal content */}
-              <div className='text-sm mb-10'>
-                <div className='space-y-2'>
-                  <p>
-                    Semper eget duis at tellus at urna condimentum mattis
-                    pellentesque lacus suspendisse faucibus interdum.
-                  </p>
-                </div>
-              </div>
-              {/* Modal footer */}
-              <div className='flex flex-wrap justify-end space-x-2'>
-                <button
-                  className='btn-sm border-slate-200 hover:border-slate-300 text-slate-600'
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    setInfoModalOpen(false);
-                  }}>
-                  Cancel
-                </button>
-                <button className='btn-sm bg-primary hover:bg-indigo-600 text-white'>
-                  Yes, Create it
-                </button>
-              </div>
-            </div>
-          </div>
-        </ModalBlank>
-        {/* End */}
-      </div>
-    );
-  };
 
   return (
     <>
@@ -496,7 +437,10 @@ function FormNewHubstar() {
               </div>
               <div className='m-1.5'>
                 <button
-                  onClick={() => prueba(setInfoModalOpen(!infoModalOpen))}
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    setInfoModalOpen(true);
+                  }}
                   type='button'
                   className='btn border-slate-200 hover:border-slate-300 text-emerald-500'>
                   Cancelar
@@ -506,6 +450,11 @@ function FormNewHubstar() {
           </form>
         </div>
       </div>
+      {/* MODAL CONFIRM */}
+      <ModalConfirm
+        setInfoModalOpen={setInfoModalOpen}
+        infoModalOpen={infoModalOpen}
+      />
     </>
   );
 }
