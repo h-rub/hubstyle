f7ac1740de4425751471494a7f6e8f8cad3f3e6c
Omar Barragan <flaky_11@hotmail.com>
2022-07-13 10:03:06 -0500

    efecto hover con color primario añadido

--- src/partials/community/UsersTilesCard.jsx
+++ src/partials/community/UsersTilesCard.jsx
@@ -51,25 +51,25 @@ function UsersTilesCard(props) {
             </header>
             {/* Menu button */}
             <EditMenu align='right' className='relative inline-flex shrink-0'>
-              <li>
+              {/* <li>
                 <Link
-                  className='font-medium text-sm text-slate-600 hover:text-slate-800 flex py-1 px-3'
+                  className='font-medium text-sm text-slate-600 hover:text-primary flex py-1 px-3'
                   to='#0'>
                   Option 1
                 </Link>
               </li>
               <li>
                 <Link
-                  className='font-medium text-sm text-slate-600 hover:text-slate-800 flex py-1 px-3'
+                  className='font-medium text-sm text-slate-600 hover:text-primary flex py-1 px-3'
                   to='#0'>
                   Option 2
                 </Link>
-              </li>
+              </li> */}
               <li>
                 <Link
-                  className='font-medium text-sm text-rose-500 hover:text-rose-600 flex py-1 px-3'
+                  className='font-medium text-sm text-rose-400 hover:text-rose-600 flex py-1 px-3'
                   to='#0'>
-                  Remove
+                  Eliminar
                 </Link>
               </li>
             </EditMenu>
@@ -83,7 +83,7 @@ function UsersTilesCard(props) {
         <div className='border-t border-slate-200'>
           <div className='flex divide-x divide-slate-200r'>
             <Link
-              className='block flex-1 text-center text-sm text-primary hover:text-indigo-600 font-medium px-3 py-4'
+              className='block flex-1 text-center text-sm text-slate-600 hover:text-primary font-medium px-3 py-4'
               to='/messages'>
               <div className='flex items-center justify-center'>
                 <svg
@@ -91,19 +91,19 @@ function UsersTilesCard(props) {
                   viewBox='0 0 16 16'>
                   <path d='M8 0C3.6 0 0 3.1 0 7s3.6 7 8 7h.6l5.4 2v-4.4c1.2-1.2 2-2.8 2-4.6 0-3.9-3.6-7-8-7zm4 10.8v2.3L8.9 12H8c-3.3 0-6-2.2-6-5s2.7-5 6-5 6 2.2 6 5c0 2.2-2 3.8-2 3.8z' />
                 </svg>
-                <span>Send Email</span>
+                <span>Enviar email</span>
               </div>
             </Link>
             <Link
-              className='block flex-1 text-center text-sm text-slate-600 hover:text-slate-800 font-medium px-3 py-4 group'
+              className='block flex-1 text-center text-sm text-slate-600 hover:text-primary font-medium px-3 py-4 group'
               to='/settings'>
               <div className='flex items-center justify-center'>
                 <svg
-                  className='w-4 h-4 fill-current text-slate-400 group-hover:text-slate-500 shrink-0 mr-2'
+                  className='w-4 h-4 fill-current text-slate-600 group-hover:text-primary shrink-0 mr-2'
                   viewBox='0 0 16 16'>
                   <path d='M11.7.3c-.4-.4-1-.4-1.4 0l-10 10c-.2.2-.3.4-.3.7v4c0 .6.4 1 1 1h4c.3 0 .5-.1.7-.3l10-10c.4-.4.4-1 0-1.4l-4-4zM4.6 14H2v-2.6l6-6L10.6 8l-6 6zM12 6.6L9.4 4 11 2.4 13.6 5 12 6.6z' />
                 </svg>
-                <span>Edit Profile</span>
+                <span>Editar Perfil</span>
               </div>
             </Link>
           </div>
