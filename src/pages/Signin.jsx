6304d1bcb8383397ccac11e8b09007176a27a7da
Omar Barragan <flaky_11@hotmail.com>
2022-07-14 17:53:42 -0500

    funcion loading agregado

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -7,6 +7,7 @@ import functionIcon from '../images/functionIcon';
 function Signin() {
   const [locked, setLocked] = useState();
   const [eye, setEye] = useState(false);
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
   const {
@@ -33,9 +34,16 @@ function Signin() {
         if (json.id === 1) {
           let result = json;
           localStorage.setItem('id', result.id);
-          navigate('/');
+          setLoading(true);
+          setTimeout(() => {
+            navigate('/');
+          }, 2000);
         } else {
-          setLocked(true);
+          setLoading(true);
+          setTimeout(() => {
+            setLocked(true);
+            setLoading(false);
+          }, 2000);
         }
       });
   }
@@ -163,11 +171,24 @@ function Signin() {
                       ¿Olvidaste tu contraseña?
                     </Link>
                   </div>
-                  <button
-                    type='submit'
-                    className='btn bg-secondary hover:bg-primary hover:text-white text-primary ml-3'>
-                    Iniciar sesión
-                  </button>
+                  {!loading ? (
+                    <button
+                      type='submit'
+                      className='btn bg-secondary hover:bg-primary hover:text-white text-primary ml-3'>
+                      Iniciar sesión
+                    </button>
+                  ) : (
+                    <button
+                      className='btn bg-primary hover:bg-indigo-600 text-white disabled:border-slate-200 disabled:bg-slate-100 disabled:text-slate-400 disabled:cursor-not-allowed shadow-none'
+                      disabled>
+                      <svg
+                        className='animate-spin w-4 h-4 fill-current shrink-0'
+                        viewBox='0 0 16 16'>
+                        <path d='M8 16a7.928 7.928 0 01-3.428-.77l.857-1.807A6.006 6.006 0 0014 8c0-3.309-2.691-6-6-6a6.006 6.006 0 00-5.422 8.572l-1.806.859A7.929 7.929 0 010 8c0-4.411 3.589-8 8-8s8 3.589 8 8-3.589 8-8 8z' />
+                      </svg>
+                      <span className='ml-2'>Loading</span>
+                    </button>
+                  )}
                 </div>
               </form>
               {/* Footer */}
--- src/pages/hubstars/UsersTiles.jsx
+++ src/pages/hubstars/UsersTiles.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Sidebar from '../../partials/Sidebar';
 import Header from '../../partials/Header';
